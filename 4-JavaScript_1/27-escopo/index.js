518e3297a0246174d4f84ae850df1d1dba9810b6
Ivan Rocha <ivan.rocha.0987@gmail.com>
2023-07-19 18:51:42 -0300

    Commit

---
+++ 4-JavaScript_1/27-escopo/index.js
@@ -0,0 +1,54 @@
+let pokemon = 'Charmander'
+
+function evoluir() {
+  pokemon = 'Charmeleon'
+}
+
+console.log(pokemon)
+evoluir()
+console.log(pokemon)
+console.log('/////')
+/*
+function criarAnimal() {
+  let animal = "gato"
+}
+criarAnimal()
+console.log(animal)
+console.log('/////')
+*/
+/*
+function avaliarNota(nota) {
+  if (nota > 60) {
+    var aprovado = true
+    let situacao = "Aprovado"
+  } else {
+    var aprovado = false
+    let situacao = "Reprovado"
+  }
+  console.log(nota)
+  console.log(aprovado)
+  console.log(situacao)
+}
+
+avaliarNota(83)
+avaliarNota(49)
+console.log('/////')
+*/
+/*
+function hallo() {
+  var texto = 'Ola mundo'
+}
+
+console.log(texto)
+console.log('/////')
+*/
+
+// Diferença de var e let
+console.log(nome)
+//console.log(sobrenome)
+
+var nome = 'Ivan'
+let sobrenome = 'Rocha'
+
+console.log(nome)
+//console.log(sobrenome)
