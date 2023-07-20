e6979d61cea2e845d5d2a0eeb5d38c33d1457e53
Ivan Rocha <ivan.rocha.0987@gmail.com>
2023-07-20 16:02:42 -0300

    Commit

--- 4-JavaScript_1/33-high-order functions e arrays/index.js
+++ 4-JavaScript_1/33-high-order functions e arrays/index.js
@@ -48,3 +48,22 @@ console.log('///////')
 
 // Reduce
 
+const nivelTotal = personagens.reduce(function (valorAcumulado, personagem) {
+  return valorAcumulado + personagem.nivel
+}, 0)
+
+console.log(nivelTotal)
+console.log('///////')
+
+const racas = personagens.reduce(function (valorAcumulado, personagem) {
+  if (valorAcumulado[personagem.raca]) {
+    valorAcumulado[personagem.raca].push(personagem)
+  } else {
+    valorAcumulado[personagem.raca] = [personagem]
+  }
+  return valorAcumulado
+}, {})
+
+console.log(racas)
+console.log('///////')
+
