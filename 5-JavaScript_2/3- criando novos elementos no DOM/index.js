85909c5e2ef32300b181ad0491e78c7b1aed1a9c
Ivan Rocha <ivan.rocha.0987@gmail.com>
2023-08-04 20:40:32 -0300

    Commit

---
+++ 5-JavaScript_2/3- criando novos elementos no DOM/index.js
@@ -0,0 +1,17 @@
+function addInput() {
+  const ul = document.getElementById('inputs')
+
+  const newLi = document.createElement('li')
+  newLi.className = 'listItem'
+  newLi.id = 'listID'
+  // Evitar usar innerHTML por questões de segurança
+  // newLi.innerHTML = '<span>item 1</span>'
+  newLi.innerText = 'Novo input: '
+
+  const newInput = document.createElement('input')
+  newInput.type = 'text'
+  newInput.name = 'input'
+
+  newLi.appendChild(newInput)
+  ul.appendChild(newLi)
+}
