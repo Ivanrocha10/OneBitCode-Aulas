d31d0b1c3bbb089acaea7be280b5d4bee2a81a41
Ivan Rocha <ivan.rocha.0987@gmail.com>
2023-09-04 20:44:26 -0300

    Trabalhando com eventos no JS

--- 5-JavaScript_2/7-trabalhando com eventos no Javascript/index.js
+++ 5-JavaScript_2/7-trabalhando com eventos no Javascript/index.js
@@ -3,25 +3,24 @@ function register(ev) {
   const sectionElement = ev.currentTarget.parentNode
   const username = sectionElement.children.username.value
   const password = sectionElement.children.password.value
-  const passwordConfirmation =
-    sectionElement.children.passwordConfirmation.value
+  const passwordConf = sectionElement.children.passwordConf.value
 
-  if (password === passwordConfirmation) {
-    alert('Usuário ' + username + ' registrado!')
+  if (password === passwordConf) {
+    alert('Usuário ' + username + ' registrado!!')
   } else {
-    alert('As senhas não conferem')
+    alert('As senhas não conferem!')
   }
-}
-
-function removeEvent() {
-  button.removeEventListener('click', register)
-  alert('Event Removed')
 }
 
 const button = document.getElementById('register-button')
 
 button.addEventListener('click', register)
 
+function removeList() {
+  button.removeEventListener('click', register)
+  alert('Event Remove')
+}
+
 button.addEventListener('mouseover', function (ev) {
-  console.log(ev)
+  console.log(ev.currentTarget)
 })
