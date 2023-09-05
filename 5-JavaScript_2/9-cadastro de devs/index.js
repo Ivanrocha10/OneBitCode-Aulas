cd17c0c146aaf2bab7ebc8068d2c9264af00af32
Ivan Rocha <ivan.rocha.0987@gmail.com>
2023-09-05 19:40:48 -0300

    Cadastro de Devs

--- 5-JavaScript_2/9-cadastro de devs/index.js
+++ 5-JavaScript_2/9-cadastro de devs/index.js
@@ -8,7 +8,24 @@ function createLabel(text, htmlFor) {
 function createInput(id, value, name, type = 'text', placeholder = '') {
   const input = document.createElement('input')
   input.id = id
-  input.valeu = value
+  input.value = value
+  input.name = name
+  input.type = type
+  input.placeholder = placeholder
+  return input
+}
+
+function createLabel(text, htmlFor) {
+  const label = document.createElement('label')
+  label.htmlFor = htmlFor
+  label.innerText = text
+  return label
+}
+
+function createInput(id, value, name, type = 'text', placeholder = '') {
+  const input = document.createElement('input')
+  input.id = id
+  input.value = value
   input.name = name
   input.type = type
   input.placeholder = placeholder
@@ -27,42 +44,25 @@ addTechBtn.addEventListener('click', function (ev) {
   const rowIndex = inputRows
   inputRows++
   newRow.id = 'inputRow-' + rowIndex
-  newRow.className = 'InputRow'
+  newRow.className = 'inputRow'
 
-  const techNameLabel = createLabel('Name: ', 'techName-' + rowIndex)
+  const techNameLabel = createLabel('Nome: ', 'techName-' + rowIndex)
   const techNameInput = createInput('techName-' + rowIndex, null, 'techName')
 
-  const expLabel = createLabel(' Experience: ')
+  const expLabel = createLabel('Experiência: ')
   const id1 = 'expRadio-' + rowIndex + '.1'
-  const expRadio1 = createInput(
-    id1,
-    '0-2 years',
-    'techExp-' + rowIndex,
-    'radio'
-  )
-  const expLabel1 = createLabel(' 0-2 years', id1)
-
+  const expRadio1 = createInput(id1, '0-2 anos', 'techExp-' + rowIndex, 'radio')
+  const expLabel1 = createLabel('0-2 anos', id1)
   const id2 = 'expRadio-' + rowIndex + '.2'
-  const expRadio2 = createInput(
-    id2,
-    '3-4 years',
-    'techExp-' + rowIndex,
-    'radio'
-  )
-  const expLabel2 = createLabel(' 3-4 years', id2)
-
+  const expRadio2 = createInput(id2, '3-4 anos', 'techExp-' + rowIndex, 'radio')
+  const expLabel2 = createLabel('3-4 anos', id2)
   const id3 = 'expRadio-' + rowIndex + '.3'
-  const expRadio3 = createInput(
-    id3,
-    '5 more years',
-    'techExp-' + rowIndex,
-    'radio'
-  )
-  const expLabel3 = createLabel(' 5 more years', id3)
+  const expRadio3 = createInput(id3, '5+ anos', 'techExp-' + rowIndex, 'radio')
+  const expLabel3 = createLabel('5+ anos', id3)
 
   const removeRowBtn = document.createElement('button')
   removeRowBtn.type = 'button'
-  removeRowBtn.innerText = 'Remove'
+  removeRowBtn.innerText = 'Remover'
   removeRowBtn.addEventListener('click', function () {
     stackInputs.removeChild(newRow)
   })
@@ -86,31 +86,26 @@ addTechBtn.addEventListener('click', function (ev) {
 form.addEventListener('submit', function (ev) {
   ev.preventDefault()
 
-  const fullNameInput = document.getElementById('fullname')
+  const fullnameInput = document.getElementById('fullname')
   const inputRows = document.querySelectorAll('.inputRow')
 
   let technologies = []
-
   inputRows.forEach(function (row) {
     // #rowId input[name="techName"]
-
     const techName = document.querySelector(
       '#' + row.id + ' input[name="techName"]'
     ).value
-
     const techExp = document.querySelector(
       '#' + row.id + ' input[type="radio"]:checked'
     ).value
-
     technologies.push({ name: techName, exp: techExp })
   })
 
-  const newDev = { fullname: fullNameInput.value, technologies: technologies }
-
+  const newDev = { fullname: fullnameInput.value, technologies: technologies }
   developers.push(newDev)
-  alert('Successful Dev Registration')
+  alert('Dev cadastrado com sucesso!')
 
-  fullNameInput.value = ''
+  fullnameInput.value = ''
   inputRows.forEach(function (row) {
     row.remove()
   })
