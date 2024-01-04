38141ba2f1dd31692607d22ef9a9885acf8cf71b
Ivan Rocha <ivan.rocha.0987@gmail.com>
2024-01-04 09:54:16 -0300

    Get and Set

--- 7-JavaScript_4/11-get and set/Wallet.js
+++ 7-JavaScript_4/11-get and set/Wallet.js
@@ -1,5 +1,7 @@
 class Wallet {
   #amount
+  #username
+
   constructor() {
     this.#amount = 100.99 * 100 // 10099
     console.log(this.#amount)
@@ -10,10 +12,27 @@ class Wallet {
   }
 
   set userName(newUserName) {
-    
+    if (typeof newUserName === 'string') {
+      this.#username = newUserName
+    } else {
+      console.log('[ERROR]')
+    }
+  }
+
+  get userName() {
+    return this.#username
   }
 }
 
 const myWallet = new Wallet()
 
 console.log(myWallet.amount)
+
+myWallet.userName = 'Ivan'
+console.log(myWallet.userName)
+
+myWallet.userName += ' Pontes'
+console.log(myWallet.userName)
+
+myWallet.userName = true
+console.log(myWallet.userName)
