937176dbff843e5c0f9829fdf425453bf76fe8d4
Ivan Rocha <ivan.rocha.0987@gmail.com>
2024-02-02 22:39:06 -0300

    Exercicio 03

---
+++ 9-TypeScript/11-exercicio03/public/dist/index.js
@@ -0,0 +1,11 @@
+const sexo = "Masculino";
+const idade = 20;
+if (sexo === "Masculino" && idade >= 18) {
+    console.log("Pode entrar");
+}
+else {
+    console.log("Não pode entrar");
+}
+function calcularIMC(peso, altura) {
+    return peso / (altura * altura);
+}
