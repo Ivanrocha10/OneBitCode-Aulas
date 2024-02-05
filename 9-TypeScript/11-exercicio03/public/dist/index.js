a708ca0dbb81b2b230e2cf7063afaa4d0f2bd154
Ivan Rocha <ivan.rocha.0987@gmail.com>
2024-02-05 15:59:26 -0300

    Exercicio 03

--- 9-TypeScript/11-exercicio03/public/dist/index.js
+++ 9-TypeScript/11-exercicio03/public/dist/index.js
@@ -1,11 +1,72 @@
-const sexo = "Masculino";
-const idade = 20;
-if (sexo === "Masculino" && idade >= 18) {
-    console.log("Pode entrar");
+const users = [];
+async function fetchUser(username) {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const user = await response.json();
+    if (user.message) {
+        alert('Usuário não encontrado!');
+    }
+    else {
+        users.push(user);
+        alert(`O usuário ${user.login} foi salvo.\n` +
+            `\nid: ${user.id}` +
+            `\nlogin: ${user.login}` +
+            `\nNome: ${user.name}` +
+            `\nBio: ${user.bio}` +
+            `\nRepositórios públicos: ${user.public_repos}`);
+    }
 }
-else {
-    console.log("Não pode entrar");
+async function showUser(username) {
+    const user = users.find(user => user.login === username);
+    if (typeof user === 'undefined') {
+        alert('Usuário não encontrado!');
+    }
+    else {
+        const response = await fetch(user.repos_url);
+        const repos = await response.json();
+        let message = `id: ${user.id}\n` +
+            `\nlogin: ${user.login}` +
+            `\nNome: ${user.name}` +
+            `\nBio: ${user.bio}` +
+            `\nRepositórios públicos: ${user.public_repos}`;
+        repos.forEach(repo => {
+            message += `\nNome: ${repo.name}` +
+                `\nDescrição: ${repo.description}` +
+                `\nEstrelas: ${repo.stargazers_count}` +
+                `\nÉ um fork: ${repo.fork ? 'Sim' : 'Não'}\n`;
+        });
+        alert(message);
+    }
 }
-function calcularIMC(peso, altura) {
-    return peso / (altura * altura);
+function showAllUser() {
+    let message = 'Usuários:\n';
+    users.forEach(user => {
+        message += `\n- ${user.login}`;
+    });
+    alert(message);
 }
+function showReposTotal() {
+    const reposTotal = users.reduce((accumulator, user) => accumulator + user.public_repos, 0);
+    alert(`Total de repositórios: ${reposTotal}`);
+}
+function showTopFive() {
+    const topFive = users.slice().sort((a, b) => b.public_repos - a.public_repos).slice(0, 5);
+    let message = 'Os cinco usuários com mais repositórios:\n';
+    topFive.forEach((user, index) => {
+        message += `\n${index + 1} - ${user.login}: ${user.public_repos} repositórios`;
+    });
+    alert(message);
+}
+async function main() {
+    await fetchUser('isaacpontes');
+    await fetchUser('julianaconde');
+    await fetchUser('pcaldass');
+    await fetchUser('lucasqueirogaa');
+    await fetchUser('frans203');
+    await fetchUser('LeDragoX');
+    await showUser('isaacpontes');
+    await showUser('julianaconde');
+    showAllUser();
+    showReposTotal();
+    showTopFive();
+}
+main();
